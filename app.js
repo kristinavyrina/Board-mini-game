72f42b6aac931c2a42181bd265d184d0422b6f13
Кристина <kv6543778@gmail.com>
2025-04-30 16:46:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,43 +8,27 @@ for (let i=0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement('div'); //создаем квадраты и кладем их в board
   square.classList.add('square'); //присваиваем класс квадрату
   
-  // square.addEventListener('mouseover', () => setColor(square)); //нормальная запись функции
   square.addEventListener('mouseover', setColor);//сокращенный вариант функции setColor()
 
-  // square.addEventListener('mouseleave', () => removeColor(square)); //нормальная запись функции
   square.addEventListener('mouseleave', removeColor);//сокращенный вариант функции removeColor()
 
   board.append(square);
 }
 
-// function setColor(element) {
-//   const color = getRandomColor();
-//   element.style.backgroundColor = color;
-//   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
-// } //нормальная запись функции setColor()
 function setColor(event) {
     const element = event.target
     const color = getRandomColor();
     element.style.backgroundColor = color;
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
-  } //сокращенный вариант функции setColor()
+  } 
   
 
-// function removeColor(element) {
-//   element.style.backgroundColor = '#1d1d1d';
-//   element.style.boxShadow = `0 0 2px #000`;
-// } //нормальная запись функции removeColor()
 function removeColor(event) {
   const element = event.target
   element.style.backgroundColor = '#1d1d1d';
   element.style.boxShadow = `0 0 2px #000`;
-} //сокращенный вариант функции removeColor()
-
-// function getRandomColor() {
-//   const index = Math.floor(Math.random() * colors.length)
-//   return colors[index];
-// } //нормальная запись функции getRandomColor()
+} 
 
 function getRandomColor() {
   return colors[Math.floor(Math.random()* colors.length)];
-} // альтернативный вариант функции getRandomColor() (улучшеный код)
+}
